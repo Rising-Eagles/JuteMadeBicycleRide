73363ca940873c7a65930c4da68e5b6138dc255e
Asus <risingeaglesltd2020@gmail.com>
2020-02-19 23:37:48 +0600

    First Structure Commit

--- app/src/main/java/com/example/jutemadebicycleride/MainActivity.java
+++ app/src/main/java/com/example/jutemadebicycleride/MainActivity.java
@@ -2,13 +2,29 @@ package com.example.jutemadebicycleride;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.example.jutemadebicycleride.UserRegistration.RegistrationActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView txt;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        txt = (TextView) findViewById(R.id.txt);
+        txt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent ds = new Intent(MainActivity.this, RegistrationActivity.class);
+                startActivity(ds);
+            }
+        });
+
     }
 }
